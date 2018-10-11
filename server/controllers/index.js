a6e93fb25095caa43e996c8fd4a774806869bede
jordanstholmes <jordanstholmes@gmail.com>
2018-10-11 12:37:37 -0700

    work on handling CORS

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,19 @@
 var models = require('../models');
+var defaultCorsHeaders = {
+  'access-control-allow-origin': '*',
+  'access-control-allow-methods': 'GET, POST, PUT, DELETE, OPTIONS',
+  'access-control-allow-headers': 'content-type, accept',
+  'access-control-max-age': 10 // Seconds.
+};
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) { // a function which handles a get request for all messages
+      res.set(defaultCorsHeaders);
+      console.log('CONTROLLER RECEIVED GET REQUEST!!!!!!!!!!!!!!!!!!!!!!!!!');
+      res.send('hello world');
+      res.end();
+    }, 
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,9 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      // res.set -> inside here get the headers object 
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
